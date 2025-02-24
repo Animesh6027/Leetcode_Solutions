e0e32416bab9e6d2f883741ed40cbf263c9e0b5e
Animesh Singh <118844122+Animesh6027@users.noreply.github.com>
2025-02-24 10:36:29 +0530

    Time: 0 ms (100%), Space: 24 MB (18.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonzeroidx=0;
+        int nonzero=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                swap(nums[i], nums[nonzeroidx]);
-                nonzeroidx++;
+                swap(nums[i], nums[nonzero]);
+                    nonzero++;
+                
             }
         }
     }
